7cf14f968e1aaf805c519fb2b60de8ca45e7a451
PhoonThetPine <thetpine254@gmail.com>
2023-09-30 10:27:54 +0800

    Initial Commit

---
+++ Hike/HikeApp.swift
@@ -0,0 +1,17 @@
+//
+//  HikeApp.swift
+//  Hike
+//
+//  Created by Phoon Thet Pine on 30/9/23.
+//
+
+import SwiftUI
+
+@main
+struct HikeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
