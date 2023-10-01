8a50c700be4a0b918041b0336c6d45919d77a62e
PhoonThetPine <thetpine254@gmail.com>
2023-10-01 22:44:22 +0800

    IOS Hike App - V1

---
+++ Hike/Screen/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  Hike
+//
+//  Created by Phoon Thet Pine on 30/9/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            CardView()
+        }
+    }
+    
+    struct ContentView_Previews: PreviewProvider {
+        static var previews: some View {
+            ContentView()
+        }
+    }
+}
